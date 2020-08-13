9e5132f5c52e558165330e787ba92ba0a829c558
zymayun4532595 <7518693+zymayun4532595@user.noreply.gitee.com>
2020-08-13 20:21:23 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,4 +7,27 @@ $.ajaxPrefilter(function(options) {
     // 在真正发起Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
 
+
+    // 统一为有权限的接口，设置headers 请求头
+    if (options.url.indeOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+            // 在 complete 回调函数中，可以使用res.responseJSON 拿到服务器响应回来的数据
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 1.强制清空 token
+                localStorage.removeItem('token')
+
+                // 2.强制跳转到登录页面
+                location.href = '/login.html'
+            }
+        }
+
+    }
+
+    
 })
